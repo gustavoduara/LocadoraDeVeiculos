66e49508b077df6c492aaa1d653cb176c20daa55
Gustavo Duara <gugaduara@gmail.com>
2025-12-14 20:44:22 -0300

    Add project files.

---
+++ LocadoraDeVeiculos.Infaestrutura.Orm/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LocadoraDeVeiculos.Infaestrutura.Orm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
